4cc08c2f02178911bd6b3b7926d652ae2bcbe518
Austin Pantoja <austinpantoja@Austins-MacBook-Air.local>
2026-01-07 00:33:49 -0700

    Removing unused methods from EmulatorApp

--- src/main/java/pantoja/chip8/emulator/EmulatorApp.java
+++ src/main/java/pantoja/chip8/emulator/EmulatorApp.java
@@ -3,17 +3,14 @@ package pantoja.chip8.emulator;
 import pantoja.chip8.ux.Keypad;
 import pantoja.chip8.ux.Window;
 
-import java.io.IOException;
-
 public class EmulatorApp {
 
-    private final Keypad keypad;
     private final Window window;
     private final Emulator emulator;
 
 
-    public EmulatorApp() throws IOException {
-        this.keypad = new Keypad();
+    public EmulatorApp() {
+        Keypad keypad = new Keypad();
         this.window = new Window(keypad, this::start);
         this.emulator = new Emulator(window, keypad);
     }
@@ -22,21 +19,5 @@ public class EmulatorApp {
     public void start() {
         window.show();
         emulator.start();
-    }
-
-
-    public void shutdown() {
-        emulator.stop();
-        window.dispose();
-    }
-
-
-    public Window getWindow() {
-        return window;
-    }
-
-
-    public Keypad getKeypad() {
-        return keypad;
     }
 }
